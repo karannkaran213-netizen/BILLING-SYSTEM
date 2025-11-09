02bf428c18247e7d332dc8666d6aa1291a2abad1
Your Name <karannkaran213@gmail.com>
2025-11-09 13:35:15 +0530

    all commit

--- restaurant_billing/settings.py
+++ restaurant_billing/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS =['.vercel.app']
 
 
 # Application definition
@@ -70,18 +70,16 @@ WSGI_APPLICATION = 'restaurant_billing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'restaurant_billing',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('railway'),
+        'USER': os.getenv('postgres'),
+        'PASSWORD': os.getenv('IfTHtZMRTKXdhOJfrLQeLGroFvNLKjen'),
+        'HOST': os.getenv('shortline.proxy.rlwy.net'),
+        'PORT': os.getenv('31784'),
     }
 }
 
