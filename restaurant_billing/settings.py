bf2904f03a5c17ca24435ef588d9fea524080615
Your Name <karannkaran213@gmail.com>
2025-11-09 14:25:48 +0530

    all code commit

--- restaurant_billing/settings.py
+++ restaurant_billing/settings.py
@@ -73,15 +73,12 @@ WSGI_APPLICATION = 'restaurant_billing.wsgi.application'
 import os
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "billing_db",
-        "USER": "postgres",
-        "PASSWORD": "123456",
-        "HOST": ".vercel.app",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # this creates a local database file
     }
 }
+
 
 
 
